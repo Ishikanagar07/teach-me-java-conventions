1bee1494677989136b4fa102971b6d061a7872ca
ishika <ishika07042000@gmail.com>
2022-03-12 19:02:00 +0530

    naming conventions updated in the source code

--- src/main/java/powerpackage/powerfinder.java
+++ src/main/java/powerpackage/powerfinder.java
@@ -1,11 +1,11 @@
 package powerpackage;
 
-public class powerfinder {
-    public static int OF(int xyz, int pqr) {
-        int p = 1;
-        for (int i = 0; i<pqr; i++) {
-            p *= xyz;
+public class PowerFinder {
+    public static int findPower(int base, int power) {
+        int tmp = 1;
+        for (int i = 0; i<power; i++) {
+            tmp *= base;
         }
-        return p;
+        return tmp;
     }
 }
